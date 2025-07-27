e95742aa46f5cf729aeced4e55711cc9099706f5
Abbas <ayanabbas727@gmail.com>
2025-07-27 17:00:55 +0530

    Adding a setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ayan Abbas",
+    author_email="ayanabbas727@gmail.com",
+    packages=find_packages()
+)
